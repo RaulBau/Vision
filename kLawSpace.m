bc7e66da30de524164c72e801fd5f9b41d43e3b6
raulbau <mrrbau@gmail.com>
2021-05-21 02:53:05 -0500

    se modifico para leer filtros hechos en paint

--- kLawSpace.m
+++ kLawSpace.m
@@ -1,6 +1,20 @@
 function [imOr] = kLawSpace(L_K, nombreA, recorta)
-  ##Se lee la imagen(en escala de grises)
-  recI =rgb2gray(imread(nombreA));
+  printf("%s\n", nombreA);
+  ##Se lee la imagen
+  img=imread(nombreA);
+  ##Obtenemos el valor de un pixel 
+  pixel=impixel(img, 10,10);
+  ##Obtenemos el tamaño del pixel
+  sPixel=size(pixel);
+  ##Se revisa si el pixel es de 3x3
+  if(sPixel(1)==3)
+    ##Se convierte la imagen a escala de grises
+    recI =rgb2gray(img);
+  else
+    ##Se iguala la imagen
+    recI=img;
+  endif
+ 
   YIni = fft2(double(recI));
  imOr = (abs(YIni).^L_K).*exp(j*angle(YIni));
 endfunction
