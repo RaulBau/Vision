65b35b7703d9fc12659946a814e496e62ce7cb7e
raulbau <mrrbau@gmail.com>
2021-05-21 01:59:09 -0500

    Se inicio con la inicialización de las mascaras

---
+++ generaMascaras.m
@@ -0,0 +1,14 @@
+##
+# Nombre: Bautista Robles Raúl
+# Clave: 229563
+# Fecha: 23/03/2021
+#
+# Se generan las mascaras
+##
+
+#Cargamos los paquetes
+pkg load image;
+pkg load video;
+
+##Filtro 1
+generaMascara(75, 195, 151,181,"8", 1290, "nesquik", "1");
